ce77d19a38bec08da9bd279e22684ccca3fa26c2
PabloBelmonte03 <pbelmontebotella@gmail.com>
2025-09-08 16:08:13 +0200

    Añadido sitio estático para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Esto permite generar archivos HTML estáticos
 };
+
 
 export default nextConfig;
